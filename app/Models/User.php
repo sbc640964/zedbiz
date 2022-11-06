718a08d155ee8ed1339f68b1193a31f2ae9571e1
simcha <sbc640964@gmail.com>
2022-11-06 14:53:34 +0200

    fix setter User password

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function password() : Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->password,
+            set: fn($value) => $this->attributes['password'] = bcrypt($value)
+        );
+    }
 }
